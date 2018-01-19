3da4aff74a2e644be03e025f375d1d3f2d608e13
Mehrnoosh <pmsarmashghi@gmail.com>
2018-01-19 16:06:30 -0500

    Update name of repo

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='Jadhav-2016-Data-Analysis',
+        name='modeling_pfc_ca1_neurons',
         version='0.1.0.dev0',
         description='Exploratory analysis of data from Jadhav 2016',
-        author='Eric Denovellis',
-        author_email='edeno@bu.edu',
+        author='',
+        author_email='',
         packages=find_packages(),
       )
