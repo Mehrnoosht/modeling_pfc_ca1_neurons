59ca3582bcbccf6cb1014b686659b676623bda80
Eric Denovellis <edeno@bu.edu>
2018-01-22 18:47:29 -0500

    Add directory information to the ANIMALS dictionary

--- src/parameters.py
+++ src/parameters.py
@@ -1,73 +1,16 @@
 from collections import namedtuple
+from os.path import join, abspath, dirname, pardir
 
+# LFP sampling frequency
 SAMPLING_FREQUENCY = 1500
+
+# Data directories and definitions
+ROOT_DIR = join(abspath(dirname(__file__)), pardir)
+RAW_DATA_DIR = join(ROOT_DIR, 'Raw-Data')
+PROCESSED_DATA_DIR = join(ROOT_DIR, 'Processed-Data')
 
 Animal = namedtuple('Animal', {'directory', 'short_name'})
 ANIMALS = {
-    'HPa': Animal(directory='HPa_direct', short_name='HPa'),
-    'HPb': Animal(directory='HPb_direct', short_name='HPb'),
-    'HPc': Animal(directory='HPc_direct', short_name='HPc')
+    'HPa': Animal(directory=join(RAW_DATA_DIR, 'HPa_direct'),
+                  short_name='HPa'),
 }
-
-
-# Multitaper Parameters
-_50Hz_Res = dict(
-    sampling_frequency=SAMPLING_FREQUENCY,
-    time_window_duration=0.020,
-    time_window_step=0.020,
-    time_halfbandwidth_product=1,
-    window_of_interest=(-0.420, 0.400)
-)
-_20Hz_Res = dict(
-    sampling_frequency=SAMPLING_FREQUENCY,
-    time_window_duration=0.050,
-    time_window_step=0.050,
-    time_halfbandwidth_product=1,
-    window_of_interest=(-0.450, 0.500)
-)
-_10Hz_Res = dict(
-    sampling_frequency=SAMPLING_FREQUENCY,
-    time_window_duration=0.100,
-    time_window_step=0.100,
-    time_halfbandwidth_product=1,
-    window_of_interest=(-0.500, 0.500)
-)
-_4Hz_Res = dict(
-    sampling_frequency=SAMPLING_FREQUENCY,
-    time_window_duration=0.250,
-    time_window_step=0.250,
-    time_halfbandwidth_product=1,
-    window_of_interest=(-0.750, 0.500)
-)
-_2Hz_Res = dict(
-    sampling_frequency=SAMPLING_FREQUENCY,
-    time_window_duration=0.500,
-    time_window_step=0.500,
-    time_halfbandwidth_product=1,
-    window_of_interest=(-1.00, 0.500)
-)
-
-MULTITAPER_PARAMETERS = {
-    '50Hz_Resolution': _50Hz_Res,
-    '20Hz_Resolution': _20Hz_Res,
-    '10Hz_Resolution': _10Hz_Res,
-    '4Hz_Resolution': _4Hz_Res,
-    '2Hz_Resolution': _2Hz_Res
-}
-
-
-RIPPLE_COVARIATES = ['session_time', 'ripple_trajectory',
-                     'ripple_direction']
-
-FREQUENCY_BANDS = {
-    'theta': (4, 12),
-    'beta': (12, 30),
-    'slow_gamma': (30, 60),
-    'mid_gamma': (60, 100),
-    'fast_gamma': (100, 125),
-    'ripple': (150, 250)
-}
-
-ALPHA = 1E-2
-
-N_DAYS = 8
